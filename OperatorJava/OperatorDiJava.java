830394584b7b31084df4b6d52cc936a5270c1c02
arcadedelux <redredemtion101@gmail.com>
2018-01-03 11:57:24 +0700

    OperatorHitungJava

---
+++ OperatorJava/OperatorDiJava.java
@@ -0,0 +1,40 @@
+public class OperatorDiJava{
+	public static void main(String[] args){
+		//Penjumlahan langsung
+		Integer value =20 + 22;
+		System.out.println("Hasil 20 + 22 Adalah =" +value);
+		
+		//Penjumlahan antar variabel
+		Integer value1 = 10;
+		Integer value2 = 12;
+		System.out.println("Hasil dari 10 + 12 Adalah = " +(value1 + value2));
+		
+		//Penguranganlangsung
+		Integer valueKurang = 22 - 20;
+		System.out.println("Hasil dari 22 - 20 Adalah = " +valueKurang);
+		
+		//Pengurangan antar variabel
+		Integer valueKurang1 = 12;
+		Integer valueKurang2 = 10;
+		System.out.println("Hasil 12 - 10 Adalah =" +(valueKurang1 - valueKurang2));
+		
+		//Pembagian langsung
+		Integer valueBagi =20/10;
+		System.out.println("Hasil Pembagian 20 / 10 Adalah= " +valueBagi);
+		
+		//Pembagian antar variable
+		Integer valueBagi1 = 100;
+		Integer valueBagi2 = 10;
+		System.out.println("Hasil Bagi 100 : 10 Adalah = " +(valueBagi1 / valueBagi2));
+		
+		//Perkalian langsung
+		Integer valueKali = 20*10;
+		System.out.println("Hasil Kali 20 x 10 Adalah =  " + valueKali);
+		
+		//Perkalian antar variabel
+		Integer valueKali1 = 1000;
+		Integer valueKali2 = 10;
+		System.out.println("Hasil Perkalian 1000 x 10 Adalah = " +(valueKali1 * valueKali2));
+		
+	}
+}
