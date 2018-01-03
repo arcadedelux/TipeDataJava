e9294a327d160d3dc6f0f4e66108a9a27b5bf3ef
arcadedelux <redredemtion101@gmail.com>
2018-01-03 11:21:52 +0700

    Comit kedua

---
+++ Index.java
@@ -0,0 +1,9 @@
+public class Index{
+	public static void main (String[] args){
+	
+	System.out.println("Welcome :" + TipeDataJava.nama);
+	System.out.println("Nomer Register :" + TipeDataJava.reg);
+	System.out.println("Tipe Ruangan :" + TipeDataJava.tipe);
+	System.out.println("Status Kepemilikan :" + TipeDataJava.in);
+	}
+}
---
+++ TipeDataJava.java
@@ -0,0 +1,6 @@
+public class TipeDataJava{
+	public static final String nama = "Arief Setya Budi";
+	public static final Integer reg = 191;
+	public static final Character tipe = 'O';
+	public static final Boolean in = true;
+}
